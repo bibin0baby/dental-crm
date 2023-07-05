87011f2fc41af5e0e0a62135151e90747812c17f
Bibin Coventry <bibincoventry@HP-Pavilion-Sleekbook-14-11e49097.localdomain>
2023-07-05 15:43:36 +0100

    fixed menu

--- resources/js/Shared/MainMenu.vue
+++ resources/js/Shared/MainMenu.vue
@@ -15,7 +15,7 @@
     <div class="mb-4">
       <Link class="group flex items-center py-3" href="/contacts">
         <icon name="users" class="mr-2 w-4 h-4" :class="isUrl('contacts') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
-        <div :class="isUrl('contacts') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Contacts</div>
+        <div :class="isUrl('contacts') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Patients</div>
       </Link>
     </div>
     <div class="mb-4">
