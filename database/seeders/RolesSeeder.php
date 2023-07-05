da2c40f5301c18b35b0ef2fd323137adfeb3b790
Bibin Coventry <bibincoventry@HP-Pavilion-Sleekbook-14-11e49097.localdomain>
2023-07-05 13:18:07 +0100

    roles updated, patient removed

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -18,7 +18,7 @@ class RolesSeeder extends Seeder
         $role_admin = Role::create(['name' => 'admin']); // IT guys
         $role_standard = Role::create(['name' => 'standard']); // Receptionist
         $role_doctor = Role::create(['name' => 'doctor']); // Doctor
-        $role_patient = Role::create(['name' => 'client']); // patients
+        //$role_patient = Role::create(['name' => 'client']); // patients
 
         $permission_read = Permission::create(['name' => 'read']);
         $permission_edit = Permission::create(['name' => 'edit']);
@@ -30,6 +30,6 @@ class RolesSeeder extends Seeder
         $role_admin->syncPermissions($permissions_admin);
         $role_standard->givePermissionTo([$permission_read, $permission_edit]);
         $role_doctor->givePermissionTo([$permission_read, $permission_edit, $permission_write]);
-        $role_patient->givePermissionTo($permission_read);
+        //$role_patient->givePermissionTo($permission_read);
     }
 }
